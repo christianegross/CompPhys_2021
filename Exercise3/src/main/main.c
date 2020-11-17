a20f860dc9e739a238cbb595b57330280dd7be88
TechnikforLife <nico.dichter@gmail.com>
2020-11-17 19:50:50 +0100

    Added documentation

--- Exercise3/src/main/main.c
+++ Exercise3/src/main/main.c
@@ -1,69 +1,15 @@
-//Exercise 1 for Computational Physics: Simulation of the 1D Ising-Model
+//Exercise 3 for Computational Physics: Simulation of the 1D Ising-Model
 //Nico Dichter, Christiane Groß
-//06. November 2020-11. Novemebr 2020
-//13. November 2020-
-//Further development: 2D, Metropolis-Hastings-algorithm instead of simple sampling
+//16. November 2020-
 
 //computation would be faster with char instead of int in the arrays, however use int at first to get human readable output
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <gsl/gsl_block.h>
-#include <gsl/gsl_matrix.h>
 #include <gsl/gsl_rng.h>//random number generator
 #include <gsl/gsl_randist.h>
 #include "math.h"//exp-Function
-#include <sys/time.h>//measuring of wallclock execution time
-
-/**
- * @fn void generate_random_state(gsl_matrix_int* lattice, gsl_rng *generator);
- * @brief uses generator to generate a random state of 1 and -1 in lattice
- *
- *
- * @param lattice	Matrix of Spins
- * @param generator	used for random states
- */
-void generate_random_state(gsl_matrix_int* lattice, gsl_rng *generator){
-	double randomstate;
-	for(int i=0;i<lattice->size1; i+=1){
-		for(int j=0;j<lattice->size2; j+=1){
-			//generate one random number for each element in lattice, decide if lattice point positive or negative
-			randomstate=gsl_rng_uniform(generator);
-			if (randomstate<0.5){
-				gsl_matrix_int_set (lattice, i, j, -1);
-			}
-			else{
-				gsl_matrix_int_set (lattice, i, j, 1);
-			}
-		}
-	}
-}
-
-
-
-/**
- * @fn double b_hamiltonian(gsl_matrix_int* lattice, double h_T, double J_T);
- * @brief Calculates the beta*hamiltonian of the given configuration, implements periodic boundary conditions with modulus
- * @note at the moment, beta=1 for alle measurements, so not considered
- *
- *
- * @param lattice	Matrix of Spins
- * @param h_T		Magnetic field strength
- * @param J_T		Coppling strength
- * 
- * @return beta*hamiltonian
- */
-double b_hamiltonian(gsl_matrix_int* lattice, double h_T, double J_T){
-	double interaction=0;
-	for(int i=0;i<lattice->size1; i++){//all rows
-		for(int j=0;j<lattice->size2; j++){//each column in row
-			interaction-=gsl_matrix_int_get (lattice, i, j)*(h_T//external magnetic field
-												+J_T*(gsl_matrix_int_get (lattice, i, (j+1)%lattice->size2)//right neighbour with periodic boundary conditions
-												+gsl_matrix_int_get (lattice, (i+1)%lattice->size1, j)));//lower neighbour with periodic boundary conditions
-		}
-	}
-	return interaction;
-}
 
 
 /**
@@ -75,16 +21,6 @@ double b_hamiltonian(gsl_matrix_int* lattice, double h_T, double J_T){
  * @return mean over all spins
  *
  */
-double mean_spin(gsl_matrix_int* lattice){
-	double sum_of_spins=0;
-	for(int i=0;i<lattice->size1; i+=1){
-		for(int j=0;j<lattice->size2; j+=1){
-			sum_of_spins+=gsl_matrix_int_get (lattice, i, j);
-		}
-	}
-	return sum_of_spins/lattice->size1/lattice->size2;
-}
-
 void leapfrog(double p_0,double phi_0,double* p_f,double* phi_f, int N_md,
 			   double J_hat_T, double h_T, int N){
 	double epsilon=1/((double)N_md);
@@ -108,13 +44,19 @@ int main(int argc, char **argv){
 	/**
 	 * Declarations:
 	 *
-	 * @var N_x_max,N_y_max		Maximum dimensions of the lattice
-	 * @var N					Current size of the lattice (side length)
-	 * @var lambda				#sites
-	 * @var h_T					Externel magnetic field/Temperatur
-	 * @var J_T					Coppling strength/Temperatur
-	 * @var therm_sweeps		#thermalization sweeps
-	 * @var amount_conf			#measurements
+	 * @var therm_steps			#thermalization steps
+	 * @var amount_conf			#configurations
+	 * @var N					Size of the lattice
+	 * @var J_T=J/T				Coppling strength
+	 * @var h_T=h/T				Strength of external field
+	 * @var p					Conjugated momentum
+	 * @var phi					Continuous paramater of H
+	 * @var delta				Relative error when going fom phi,p->phi',p'
+	 * @var prob				Probability to accept a proposed phi'
+	 * @var N_md				#steps in leap frog
+	 * @var mean_magnetization	mean magnetization
+	 * @var accept_rate			acceptance rate
+	 * @var amount_ar			amount of accept/reject steps
 	 */
 	
 	int therm_steps=500;
@@ -124,7 +66,16 @@ int main(int argc, char **argv){
 	double J_T=0.2;
 	double J_hat_T=J_T/N;
 	double h_T=0.5;
-	
+	double p_0=1;
+	double phi_0=2;
+	double p_f=0;
+	double phi_f=0;
+	double delta=0;
+	double prob=1;
+	int N_md=4;
+	double mean_magnetization=0;
+	double accept_rate=0;
+	int amount_ar=0;
 	
 	//set and allocate random number generator
 	int seed=2;//use fixed seed: result should be exactly reproduced using the same seed
@@ -134,7 +85,6 @@ int main(int argc, char **argv){
 	
 	/**
 	 * @note	Allocate blocks for several measurements
-	 * 			and allocate memory for the largest lattice and set its initial state randomly
 	 */
 	gsl_block * set_of_phis=gsl_block_alloc (amount_conf);
 	gsl_block * magnetizations=gsl_block_alloc (amount_conf);
@@ -147,16 +97,10 @@ int main(int argc, char **argv){
 	FILE * raw_data=fopen ("data/raw.dat", "w");
 	fprintf(raw_data,"#N\tJ\t<m>\n");
 	
-	double p_0=1;
-	double phi_0=2;
-	double p_f=0;
-	double phi_f=0;
-	double delta=0;
-	double prob=1;
-	int N_md=4;
-	double mean_magnetization=0;
-	double accept_rate=0;
-	int amount_ar=0;
+	
+	/**
+	 * @note	Test rather the convergence conditions for leap frog are met
+	 */
 	double H_0=art_hamiltonian (p_0, phi_0,  J_hat_T,  h_T,  N);
 	for(int i=2;i<101;i++){
 		leapfrog (p_0, phi_0, &p_f, &phi_f, i,  J_hat_T, h_T,  N);
@@ -164,10 +108,20 @@ int main(int argc, char **argv){
 		fprintf (converge_data,"%d\t%.10f\n",i , fabs(delta));
 		
 	}
+	/**
+	 * @note	Itterate through different sets of N/J
+	 */
 	for(N=5;N<21;N+=5){
 		for(J_T=0.2;J_T<2.0001;J_T+=0.01){
 			J_hat_T=J_T/((double)N);
 			mean_magnetization=0;
+			
+			/**
+			 * @note	Thermalization:
+			 * 			1. sample p
+			 * 			2. leapfrog
+			 * 			3. accept/reject
+			 */
 			for(int i=0;i<therm_steps;i++){
 				p_0=gsl_ran_ugaussian (generator);
 				H_0=art_hamiltonian (p_0, phi_0,  J_hat_T,  h_T,  N);
@@ -177,7 +131,13 @@ int main(int argc, char **argv){
 					phi_0=phi_f;
 				}
 			}
-			
+			/**
+			 * @note	Generate ensemble of phis:
+			 * 			1. sample p
+			 * 			2. leapfrog
+			 * 			3. accept/reject
+			 * 			And take measurements for <m> and <epsilon>
+			 */
 			for(int i=0;i<amount_conf;i++){
 				p_0=gsl_ran_ugaussian (generator);
 				H_0=art_hamiltonian (p_0, phi_0,  J_hat_T,  h_T,  N);
@@ -197,6 +157,9 @@ int main(int argc, char **argv){
 		}
 	}
 	
+	/**
+	 * @note	Print out the overall acceptance rate
+	 */
 	accept_rate/=amount_ar;
 	printf ("Acceptance Rate:%f\n",accept_rate);
 	
