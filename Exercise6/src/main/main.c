23c743287fe750a4a3bceba251c91bf1cbc2468f
christianegross <chgross.wickede@gmail.com>
2020-12-12 18:56:32 +0100

    added data for wavefunctions from eCampus, as well as function to read in data from file

--- Exercise6/src/main/main.c
+++ Exercise6/src/main/main.c
@@ -10,9 +10,37 @@
 #include "math.h"//exp-Function
 #include <gsl/gsl_vector.h>
 
-
+void readinwavefunction(FILE * wavefunctionfile, gsl_vector *p, gsl_vector *w, gsl_vector* wf){
+	/**
+	 * @note cannot use built-in function because there are lines with other data, and several columns
+	 * */
+	 double pread, wread, wfread, empty;
+	 rewind(wavefunctionfile);
+	 fscanf(wavefunctionfile, "# Lam=   %le  A=  %le  mb=   %le C0=  %le \n# p [fm-1]       w             wf [fm3/2]\n", &empty, &empty, &empty, &empty);
+	 for (int line=0; line<p->size; line+=1){
+		 if(3== fscanf(wavefunctionfile, "%le%le%le\n", &pread, &wread, &wfread)){
+		 gsl_vector_set(p, line, pread);
+		 gsl_vector_set(w, line, wread);
+		 gsl_vector_set(wf, line, wfread);
+		 //printf("sucessfully read\n");
+	 }
+	 }
+}
 
 int main(int argc, char **argv){
+	FILE *wavefunctionfile=fopen("data/wavefunctions/wf-obe-lam=0300.00.dat", "r");
+	FILE *testfile=fopen("data/test.dat", "w");
+	gsl_vector *p=gsl_vector_alloc(60);
+	gsl_vector *w=gsl_vector_alloc(60);
+	gsl_vector *wf=gsl_vector_alloc(60);
+	readinwavefunction(wavefunctionfile, p, w, wf);
+	gsl_vector_fprintf(testfile, p, "%e");
+	fclose(wavefunctionfile);
+	fclose(testfile);
+	gsl_vector_free(p);
+	gsl_vector_free(w);
+	gsl_vector_free(wf);
+	
 	
 	return 0;
 }
