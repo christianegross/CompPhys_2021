29f2fe8aee97a4b633eaf6f53b9c9c9b2434a88e
christianegross <chgross.wickede@gmail.com>
2020-12-12 17:55:44 +0100

    Initial Commit Exercise 6: empty templates

---
+++ Exercise6/src/main/main.c
@@ -0,0 +1,20 @@
+//Exercise 6 for Computational Physics: Form factor of a boson bound state
+//Nico Dichter, Christiane Groß
+//12. December 2020-
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <gsl/gsl_block.h>
+#include <gsl/gsl_rng.h>//random number generator
+#include <gsl/gsl_randist.h>//pull random number from gaussian
+#include "math.h"//exp-Function
+#include <gsl/gsl_vector.h>
+
+
+
+int main(int argc, char **argv){
+	
+	return 0;
+}
+
+
