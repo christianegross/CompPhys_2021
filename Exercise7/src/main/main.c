f8dcc32a41b730d7d3d5abbec58ccd1596dfa01e
christianegross <chgross.wickede@gmail.com>
2020-12-18 10:42:19 +0100

    start exercise 7: empty files copied

---
+++ Exercise7/src/main/main.c
@@ -0,0 +1,23 @@
+//Exercise 6 for Computational Physics: Form factor of a boson bound state
+//Nico Dichter, Christiane Groß
+//18. December 2020-
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <gsl/gsl_block.h>
+#include <gsl/gsl_rng.h>//random number generator
+#include <gsl/gsl_randist.h>//pull random number from gaussian
+#include "math.h"//exp-Function
+#include <gsl/gsl_vector.h>
+#include <gsl/gsl_interp.h>
+#include <gsl/gsl_spline.h>
+#include <gsl/gsl_sf_legendre.h>
+#include <gsl/gsl_integration.h>
+
+
+int main(int argc, char **argv){
+	
+	return 0;
+}
+
+
