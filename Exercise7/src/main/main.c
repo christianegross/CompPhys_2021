82d6e4e1c242fbad4f82ac5dc09d62bf26506f46
christianegross <chgross.wickede@gmail.com>
2020-12-18 15:32:39 +0100

    added function for filling matrix a as given on sheet, determined t using the gsl_blas library for inverting matrices, tested functions, determined |s|

--- Exercise7/src/main/main.c
+++ Exercise7/src/main/main.c
@@ -14,6 +14,9 @@
 #include <gsl/gsl_spline.h>
 #include <gsl/gsl_sf_legendre.h>
 #include <gsl/gsl_integration.h>
+#include <gsl/gsl_complex.h>
+#include <gsl/gsl_complex_math.h>
+#include <gsl/gsl_blas.h>
 
 /**
  * @brief determines Gauss-Legendre integration points and weights for given grid size, also sets additional point pN=q with weight 1
@@ -36,7 +39,7 @@ void getgridpoints(gsl_vector *momenta, gsl_vector *weights, double q, double pm
  * @brief uses Gauss-Legendre integration to calculate the potential, form of the potential given in lecture 7, 
  * fills matrix with the potential
  * */
-inline void fillpotentialmatrix(gsl_matrix *pot, gsl_vector *momenta, int l, int sizeofangulargrid, double mu){
+inline void fillpotentialmatrix(gsl_matrix_complex *pot, gsl_vector *momenta, int l, int sizeofangulargrid, double mu){
 	gsl_integration_glfixed_table *table=gsl_integration_glfixed_table_alloc(sizeofangulargrid);
 	double result, x, weight, pi, pj;
 	int errorcode;
@@ -49,10 +52,46 @@ inline void fillpotentialmatrix(gsl_matrix *pot, gsl_vector *momenta, int l, int
 				errorcode=gsl_integration_glfixed_point(-1, 1, k, &x, &weight, table);
 				result+=weight*gsl_sf_legendre_Pl(l, x)/(pi*pi+pj*pj-2*pi*pj*x+mu*mu);
 			}
-			gsl_matrix_set(pot, i, j, 2*M_PI*result);
+			gsl_matrix_complex_set(pot, i, j, gsl_complex_rect(2*M_PI*result, 0));
 		}
 	}
 	gsl_integration_glfixed_table_free(table);	
+}
+
+/**
+ * @brief determines elements of matrix A as given on sheet
+ * */
+inline void fillmatrixa(gsl_matrix_complex *a, gsl_matrix_complex *pot, gsl_vector *momenta, gsl_vector *weights, double mu, double pmax){
+	double result, resultimaginary, pk, pm;
+	double q=gsl_vector_get(momenta, momenta->size-1);
+	for (int i=0; i<a->size1; i+=1){
+		//k!=N
+		for (int k=0; k<a->size2-1; k+=1){
+			result=0;
+			pk=gsl_vector_get(momenta, k);
+			if(i==k){result+=1;}
+			result-=2*mu*GSL_REAL(gsl_matrix_complex_get(pot, i, k))*pk*pk*gsl_vector_get(weights, k)/(q*q-pk*pk);
+			gsl_matrix_complex_set(a, i, k, gsl_complex_rect(result, 0));
+		}
+		//k==N
+		result=0;
+		if(i==a->size2-1){result+=1;}
+		for (int m=0; m<a->size2-1; m+=1){
+			pm=gsl_vector_get(momenta, m);
+			result+=2*mu*GSL_REAL(gsl_matrix_complex_get(pot, i, m))*pm*pm*gsl_vector_get(weights, m)/(q*q-pm*pm);
+		}
+		result-=mu*q*GSL_REAL(gsl_matrix_complex_get(pot, i, pot->size2-1))*log((pmax+q)/(pmax-q));
+		resultimaginary=M_PI*mu*q*GSL_REAL(gsl_matrix_complex_get(pot, i, pot->size2-1));
+		gsl_matrix_complex_set(a, i, a->size2-1, gsl_complex_rect(result, resultimaginary));
+	}
+} 
+
+/**
+ * @brief calculates the modulus of s
+ * */
+inline double absofs(gsl_complex tnn, double mu, double q){
+	gsl_complex result=gsl_complex_add_real(gsl_complex_mul_imag(gsl_complex_mul_real(tnn, 2*M_PI*mu*q), -1), 1);
+	return gsl_complex_abs(result);
 }
 
 int main(int argc, char **argv){
@@ -61,11 +100,12 @@ int main(int argc, char **argv){
 	 * */
 	int sizeofgrid=20;
 	int sizeofangulargrid=20;
-	int l=5;
+	int l=0;
 	double E=1;
 	double mu=938.92;
 	double q=sqrt(2*mu*E);
 	double pmax=100;
+	gsl_complex tnn;
 	
 	/**
 	 * @note set up gsl vectors and matrices, streams for toring results
@@ -73,7 +113,9 @@ int main(int argc, char **argv){
 	gsl_vector *momenta=gsl_vector_alloc(sizeofgrid+1);
 	gsl_vector *weights=gsl_vector_alloc(sizeofgrid+1);
 	
-	gsl_matrix *pot=gsl_matrix_alloc(sizeofgrid+1, sizeofgrid+1);
+	gsl_matrix_complex *pot=gsl_matrix_complex_alloc(sizeofgrid+1, sizeofgrid+1);
+	gsl_matrix_complex *a=gsl_matrix_complex_alloc(sizeofgrid+1, sizeofgrid+1);
+	gsl_matrix_complex *t=gsl_matrix_complex_alloc(sizeofgrid+1, sizeofgrid+1);
 	
 	FILE *test=fopen("data/test.dat", "w");
 	
@@ -83,23 +125,51 @@ int main(int argc, char **argv){
 	
 	getgridpoints(momenta, weights, q, pmax, sizeofgrid);
 	
-	gsl_vector_fprintf(test, momenta, "%e");
-	fprintf(test, "\n\n");
-	gsl_vector_fprintf(test, weights, "%e");
-	fprintf(test, "\n\n");
+	//~ gsl_vector_fprintf(test, momenta, "%e");
+	//~ fprintf(test, "\n\n");
+	//~ gsl_vector_fprintf(test, weights, "%e");
+	//~ fprintf(test, "\n\n");
 	
 	fillpotentialmatrix(pot, momenta, l, sizeofangulargrid, mu);
 	
-	gsl_matrix_fprintf(test, pot, "%e");
+	//~ gsl_matrix_complex_fprintf(test, pot, "%e");
+	//~ fprintf(test, "\n\n");
+	
+	fillmatrixa(a, pot, momenta, weights, mu, pmax);
+	
+	//~ gsl_matrix_complex_fprintf(test, a, "%e");
+	//~ fprintf(test, "\n\n");
+	
+	/**
+	 * @note determine t by using t=A^-1*V, copy V in empty t so V does not get lost
+	 * */
+	gsl_matrix_complex_memcpy(t, pot);
+	
+	gsl_matrix_complex_fprintf(test, t, "%e");
 	fprintf(test, "\n\n");
 	
+	gsl_blas_ztrsm(CblasLeft, CblasUpper, CblasNoTrans, CblasNonUnit, gsl_complex_rect(1.0, 0), a, t);
+	
+	gsl_matrix_complex_fprintf(test, t, "%e");
+	fprintf(test, "\n\n");
+	
+	/**
+	 * @note determine t_NN, check if |S|==1
+	 * */
+	tnn=gsl_matrix_complex_get(t, t->size1-1, t->size2-1);
+	printf("%e\n", GSL_REAL(tnn));
+	//~ s=gsl_complex_sub_real(gsl_complex_mul_imag(gsl_complex_mul_real(gsl_matrix_complex_get(t, t->size1-1, t->size2-1), 2*M_PI*mu*q), -1), 1);
+	//~ printf("%e\n", gsl_complex_abs(s));
+	printf("%e\n", absofs(tnn, mu, q));
 	/**
 	 * @note cleanup
 	 * */
 	
 	gsl_vector_free(momenta);
 	gsl_vector_free(weights);
-	gsl_matrix_free(pot);
+	gsl_matrix_complex_free(pot);
+	gsl_matrix_complex_free(a);
+	fclose(test);
 	
 	return 0;
 }
