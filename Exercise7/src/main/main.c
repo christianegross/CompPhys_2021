82b03c45b55bc9b7181a6ba19697dc2a9b4bcbde
christianegross <chgross.wickede@gmail.com>
2020-12-18 11:30:16 +0100

    added and tested functions for getting momentum and weight grid, potential matrix

--- Exercise7/src/main/main.c
+++ Exercise7/src/main/main.c
@@ -9,13 +9,85 @@
 #include <gsl/gsl_randist.h>//pull random number from gaussian
 #include "math.h"//exp-Function
 #include <gsl/gsl_vector.h>
+#include <gsl/gsl_matrix.h>
 #include <gsl/gsl_interp.h>
 #include <gsl/gsl_spline.h>
 #include <gsl/gsl_sf_legendre.h>
 #include <gsl/gsl_integration.h>
 
+void getgridpoints(gsl_vector *momenta, gsl_vector *weights, double q, double pmax, int sizeofgrid){
+	gsl_integration_glfixed_table *table=gsl_integration_glfixed_table_alloc(sizeofgrid);
+	double momentum, weight;
+	int errorcode;
+	for (int k=0; k<sizeofgrid; k+=1){
+		errorcode=gsl_integration_glfixed_point(0, pmax, k, &momentum, &weight, table);
+		gsl_vector_set(momenta, k, momentum);
+		gsl_vector_set(weights, k, weight);
+	}
+	gsl_vector_set(momenta, momenta->size-1, q);
+	gsl_vector_set(weights, weights->size-1, 1);
+	gsl_integration_glfixed_table_free(table);	
+}
+
+inline double potential(double pa, double pb, int l, int sizeofangulargrid, double mu, gsl_integration_glfixed_table *table){
+	double result=0, x, weight;
+	int errorcode;
+	for (int k=0; k<sizeofangulargrid; k+=1){
+		errorcode=gsl_integration_glfixed_point(-1, 1, k, &x, &weight, table);
+		result+=weight*gsl_sf_legendre_Pl(l, x)/(pa*pa+pb*pb-2*pa*pb*x+mu*mu);
+	}
+	return 2.0*M_PI*result;
+}
+
+inline void fillpotentialmatrix(gsl_matrix *pot, gsl_vector *momenta, int l, int sizeofangulargrid, double mu){
+	gsl_integration_glfixed_table *table=gsl_integration_glfixed_table_alloc(sizeofangulargrid);
+	double result, x, weight, pi, pj;
+	int errorcode;
+	for (int i=0; i<pot->size1; i+=1){
+		pi=gsl_vector_get(momenta, i);
+		for (int j=0; j<pot->size2; j+=1){
+			pj=gsl_vector_get(momenta, j);
+			result=0;
+			for (int k=0; k<sizeofangulargrid; k+=1){
+				errorcode=gsl_integration_glfixed_point(-1, 1, k, &x, &weight, table);
+				result+=weight*gsl_sf_legendre_Pl(l, x)/(pi*pi+pj*pj-2*pi*pj*x+mu*mu);
+			}
+			gsl_matrix_set(pot, i, j, 2*M_PI*result);
+		}
+	}
+	gsl_integration_glfixed_table_free(table);	
+}
 
 int main(int argc, char **argv){
+	int sizeofgrid=20;
+	int sizeofangulargrid=20;
+	int l=5;
+	double E=1;
+	double mu=938.92;
+	double q=sqrt(2*mu*E);
+	double pmax=100;
+	gsl_vector *momenta=gsl_vector_alloc(sizeofgrid+1);
+	gsl_vector *weights=gsl_vector_alloc(sizeofgrid+1);
+	
+	gsl_matrix *pot=gsl_matrix_alloc(sizeofgrid+1, sizeofgrid+1);
+	
+	FILE *test=fopen("data/test.dat", "w");
+	
+	getgridpoints(momenta, weights, q, pmax, sizeofgrid);
+	
+	gsl_vector_fprintf(test, momenta, "%e");
+	fprintf(test, "\n\n");
+	gsl_vector_fprintf(test, weights, "%e");
+	fprintf(test, "\n\n");
+	
+	fillpotentialmatrix(pot, momenta, l, sizeofangulargrid, mu);
+	
+	gsl_matrix_fprintf(test, pot, "%e");
+	fprintf(test, "\n\n");
+	
+	gsl_vector_free(momenta);
+	gsl_vector_free(weights);
+	gsl_matrix_free(pot);
 	
 	return 0;
 }
