134aad87e36ec775f801e0f2c126350c64bb4dbe
christianegross <chgross.wickede@gmail.com>
2021-02-04 13:06:08 +0100

    added hermitian Matrices, tried orthonormalization

--- Project/src/main/main.c
+++ Project/src/main/main.c
@@ -19,15 +19,24 @@
 
 int main(int argc, char **argv){
 	double a,b,c,d,det;
+	double epsilon=1;
+	double e,f,g,h, det;
+	gsl_complex complexproduct;
 	gsl_matrix_complex *one=gsl_matrix_complex_alloc(2,2);
 	gsl_matrix_complex *two=gsl_matrix_complex_alloc(2,2);
+	gsl_matrix_complex *three=gsl_matrix_complex_alloc(2,2);
+	gsl_matrix_complex *four=gsl_matrix_complex_alloc(2,2);
 	gsl_matrix_complex *multiplied=gsl_matrix_complex_alloc(2,2);	
+	gsl_vector_complex_view columnzero, columnone;
 	int seed=2;//use fixed seed: result should be exactly reproduced using the same seed
 	gsl_rng *generator;
 	generator=gsl_rng_alloc(gsl_rng_mt19937);//use mersenne-twister
 	gsl_rng_set(generator, seed);
 	
-	for(int i=1;i<50;i+=1){	
+	for(int i=1;i<10;i+=1){	
+		 /**
+		  * choose four random doubles between -1 and 1, normalise through determinant of future matrix, so determinant is 1
+		  * */
 		a=2.0*gsl_rng_uniform(generator)-1.0;
 		b=2.0*gsl_rng_uniform(generator)-1.0;
 		c=2.0*gsl_rng_uniform(generator)-1.0;
@@ -38,6 +47,7 @@ int main(int argc, char **argv){
 		c/=sqrt(det);
 		d/=sqrt(det);
 		
+		/** set complex hermitian matrix*/
 		gsl_matrix_complex_set(one, 0, 0, gsl_complex_rect(a, b));
 		gsl_matrix_complex_set(one, 1, 1, gsl_complex_rect(a,-b));
 		gsl_matrix_complex_set(one, 0, 1, gsl_complex_rect(c, d));
@@ -47,7 +57,7 @@ int main(int argc, char **argv){
 		gsl_matrix_complex_set(two, 1, 1, gsl_complex_rect(a, b));
 		gsl_matrix_complex_set(two, 0, 1, gsl_complex_rect(-c,-d));
 		gsl_matrix_complex_set(two, 1, 0, gsl_complex_rect(c,-d));
-		
+		/** see if uudagger =1*/
 		gsl_blas_zgemm(CblasNoTrans, CblasNoTrans, gsl_complex_rect(1,0), one, two, gsl_complex_rect(0,0), multiplied);
 		
 		printf("run no. %d\n matrix one \n", i);
@@ -57,7 +67,51 @@ int main(int argc, char **argv){
 		printf("\n multiplied\n");
 		gsl_matrix_complex_fprintf(stdout, multiplied, "%f");
 		printf("\n old and new determinant\n");
-		printf("%f\t%f\n\n", det, a*a+b*b+c*c+d*d); 
+		printf("%f\t%f\n\n", det, a*a+b*b+c*c+d*d);
+		
+		/**
+		 * Make hermitian Matrix 3 with entries between -1 and 1 {{e, f+ig},{f-ig, h}}
+		 * Fill Matrix four =1+iepsilon*three={{1+iepsilon e, -epsilon g+iepsilon f}, {epsilong+iepsilon f, 1+iepsilon h}}
+		 * */  
+		
+		e=2.0*gsl_rng_uniform(generator)-1.0;
+		f=2.0*gsl_rng_uniform(generator)-1.0;
+		g=2.0*gsl_rng_uniform(generator)-1.0;
+		h=2.0*gsl_rng_uniform(generator)-1.0;
+		
+		gsl_matrix_complex_set(three, 0, 0, gsl_complex_rect(e, 0));
+		gsl_matrix_complex_set(three, 1, 1, gsl_complex_rect(h, 0));
+		gsl_matrix_complex_set(three, 0, 1, gsl_complex_rect(f, g));
+		gsl_matrix_complex_set(three, 1, 0, gsl_complex_rect(f,-g));
+		
+		gsl_matrix_complex_set(four, 0, 0, gsl_complex_rect(1, epsilon*e));
+		gsl_matrix_complex_set(four, 1, 1, gsl_complex_rect(1, epsilon*h));
+		gsl_matrix_complex_set(four, 0, 1, gsl_complex_rect(-epsilon*g, epsilon*f));
+		gsl_matrix_complex_set(four, 1, 0, gsl_complex_rect(epsilon*g, epsilon*f));
+		
+		
+		gsl_matrix_complex_fprintf(stdout, four, "%f");
+		
+		columnzero=gsl_matrix_complex_column(four, 0);
+		columnone=gsl_matrix_complex_column(four, 1);
+		
+		det=gsl_blas_dznrm2(&columnzero.vector);
+		gsl_blas_zdscal(1.0/det, &columnzero.vector);
+		
+		gsl_blas_zdotu(&columnzero.vector, &columnone.vector, &complexproduct);
+		gsl_blas_zaxpy(gsl_complex_mul_real(complexproduct, -1.0), &columnzero.vector, &columnone.vector);
+		
+		det=gsl_blas_dznrm2(&columnone.vector);
+		gsl_blas_zdscal(1.0/det, &columnone.vector);
+		
+		printf("\n%f\n", gsl_blas_dznrm2(&columnzero.vector));
+		printf("%f\n", gsl_blas_dznrm2(&columnone.vector));
+		gsl_matrix_complex_fprintf(stdout, four, "%f");
+		
+		complexproduct=gsl_complex_add(gsl_complex_mul(gsl_matrix_complex_get(four, 0,0), gsl_matrix_complex_get(four, 1,1)), gsl_complex_mul_real(gsl_complex_mul(gsl_matrix_complex_get(four, 1,0), gsl_matrix_complex_get(four, 1,0)), -1.0));
+		
+		gsl_blas_zdotu(&columnzero.vector, &columnone.vector, &complexproduct);
+		printf("\n%f+%f*i\n", GSL_REAL(complexproduct), GSL_IMAG(complexproduct));
 	}
 	
 	gsl_rng_free(generator);
