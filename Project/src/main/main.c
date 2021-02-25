01fc74bffbeb5c316df39b289e831bbb9951cffe
christianegross <chgross.wickede@gmail.com>
2021-02-25 13:37:19 +0100

    added function calculategamma, to make sweep less confusing. Also added 'backwards' loop in which U_mu(x) is also involved to gamma. Added function calculateplaquette to calculate a single plaquette. Compare if results if expectationvalue of plaquette is calculated on its own after each sweep to plaquette calculated during the sweep, at first glance it does not seem to make a difference. Introduced inverse temperature beta instead of coupling constant to action.

--- Project/src/main/main.c
+++ Project/src/main/main.c
@@ -99,10 +99,39 @@ inline double deltaS(gsl_matrix_complex *deltacontribution, gsl_matrix_complex* 
 	return 0.5*GSL_REAL(trace(helptwo));
 }
 
+void calculateGamma(int * neighbour, int counter, int mu, int size, gsl_matrix_complex *plaquettecontribution, gsl_matrix_complex *deltacontribution, gsl_matrix_complex *helpone, gsl_matrix_complex *helptwo, gsl_matrix_complex **matrixarray){
+	/**
+	* calculate contribution of different plaquettes to action
+	*forward U_nu(x+amu)*U^dagger_mu(x+anu)*U^dagger_nu(x)
+	*backward U^dagger_nu(x+amu-anu)*U^dagger_mu(x-anu)*U_nu(x-anu) 
+	* */
+	for (int nu =0;nu<4;nu+=1){ if(mu!=nu){
+		//forward plaquette
+		gsl_blas_zgemm(CblasNoTrans, CblasConjTrans, GSL_COMPLEX_ONE, matrixarray[counter+neighbour[2*mu]-mu+nu], matrixarray[counter+neighbour[2*nu]], GSL_COMPLEX_ZERO, helpone);
+		gsl_blas_zgemm(CblasNoTrans, CblasConjTrans, GSL_COMPLEX_ONE, helpone, matrixarray[counter-mu+nu] , GSL_COMPLEX_ZERO, helptwo);
+		gsl_matrix_complex_add(deltacontribution, helptwo);
+		//~ fprintf(stdout, "%.1f\t", GSL_REAL(det2(helptwo)));
+		if(mu>nu){gsl_matrix_complex_add(plaquettecontribution, helptwo);}
+		//backward plaquette
+		gsl_blas_zgemm(CblasConjTrans, CblasConjTrans, GSL_COMPLEX_ONE, matrixarray[counter+neighbour[2*mu]+neighbour[2*nu+1]-mu+nu], matrixarray[counter+neighbour[2*nu+1]], GSL_COMPLEX_ZERO, helpone);
+		gsl_blas_zgemm(CblasNoTrans, CblasNoTrans, GSL_COMPLEX_ONE, helpone, matrixarray[counter+neighbour[2*nu+1]-mu+nu] , GSL_COMPLEX_ZERO, helptwo);
+		gsl_matrix_complex_add(deltacontribution, helptwo);
+	}}
+}
+
+double calculateplaquette(gsl_matrix_complex ** matrixarray, int counter, int* neighbour, int mu, int nu, gsl_matrix_complex *helpone, gsl_matrix_complex *helptwo, gsl_matrix_complex *helpthree){
+	gsl_blas_zgemm(CblasNoTrans, CblasNoTrans, GSL_COMPLEX_ONE, matrixarray[counter+mu], matrixarray[counter+neighbour[2*mu]+nu], GSL_COMPLEX_ZERO, helpone);
+	gsl_blas_zgemm(CblasConjTrans, CblasConjTrans, GSL_COMPLEX_ONE, matrixarray[counter+neighbour[2*nu]+mu], matrixarray[counter+nu], GSL_COMPLEX_ZERO, helptwo);
+	gsl_blas_zgemm(CblasNoTrans, CblasNoTrans, GSL_COMPLEX_ONE, helpone, helptwo, GSL_COMPLEX_ZERO, helpthree);
+	return 0.5*GSL_REAL(trace(helpthree));
+}
+	
+
 int main(int argc, char **argv){
 	//set up constants, matrices, generator
 	double epsilon=0.2;
 	int size=8;
+	double beta=2.3;
 	int numberofthermalizations=100;
 	int numberofmeasurements=2048; //=pow(2, 13)
 	
@@ -141,126 +170,120 @@ int main(int argc, char **argv){
 	
 	//test of MH: go through lattice, perform 10 accept/reject steps at every link, measure acceptance rate
 	/**thermalizations**/
+	/**neighbours: need two neighbours for every direction, one forward and one backward
+	 * 0: t-forward	1:t-backward
+	 * 2: t-forward	3:t-backward
+	 * 4: t-forward	5:t-backward
+	 * 6: t-forward	7:t-backward
+	 * forward: 2*mu, backward, 2*mu+1
+	 * **/
 	int counter, acceptance;
-	int neighbour[4]; //for implementing periodic boundary conditions
-	double plaquetteexpectation;
+	int neighbour[8]; //for implementing periodic boundary conditions
+	double plaquetteexpectation, plaquetteafter;
 	for(int runs=0;runs<numberofthermalizations;runs+=1){
 		acceptance=0;
 		plaquetteexpectation=0;
+		plaquetteafter=0;
 		for (int x=0;x<size;x+=1){
-			neighbour[0]=(x==size-1)?-(size-1)*pow(size, 3)*4:pow(size,3)*4;
+			neighbour[6]=(x==size-1)?-(size-1)*pow(size, 3)*4:pow(size,3)*4;
+			neighbour[7]=(x==0)?(size-1)*pow(size, 3)*4:-pow(size,3)*4;
 			for (int y=0;y<size;y+=1){
-				neighbour[1]=(y==size-1)?-(size-1)*pow(size, 2)*4:pow(size,2)*4;
+				neighbour[4]=(y==size-1)?-(size-1)*pow(size, 2)*4:pow(size,2)*4;
+				neighbour[5]=(y==0)?(size-1)*pow(size, 2)*4:-pow(size,2)*4;
 				for (int z=0;z<size;z+=1){
 					neighbour[2]=(z==size-1)?-(size-1)*size*4:size*4;
+					neighbour[3]=(z==0)?(size-1)*size*4:-size*4;
 					for (int t=0;t<size;t+=1){
-						neighbour[3]=(x==size-1)?-(size-1)*4:4;
+						neighbour[0]=(t==size-1)?-(size-1)*4:4;
+						neighbour[1]=(t==0)?(size-1)*4:-4;
 						for (int mu=0;mu<4;mu+=1){
 							counter=x*size*size*size*4+y*size*size*4+z*size*4+t*4+mu;
 							//~ fprintf(stdout, "%d\n", counter);
 							settozero(plaquettecontribution); settozero(deltacontribution);
-							/**calculate contribution of different plaquettes to action
-							*U_nu(x+amu)*U^dagger_mu(x+anu)*U^dagger_nu(x)*/
-							for (int nu =0;nu<4;nu+=1){
-								if(mu!=nu){
-								gsl_blas_zgemm(CblasNoTrans, CblasConjTrans, GSL_COMPLEX_ONE, matrixarray[counter-mu+neighbour[mu]+nu], matrixarray[counter+neighbour[nu]], GSL_COMPLEX_ZERO, helpone);
-								gsl_blas_zgemm(CblasNoTrans, CblasConjTrans, GSL_COMPLEX_ONE, helpone, matrixarray[counter-mu+nu] , GSL_COMPLEX_ZERO, helptwo);
-								gsl_matrix_complex_add(deltacontribution, helptwo);
+							//~ /**calculate contribution of different plaquettes to action
+							//~ *U_nu(x+amu)*U^dagger_mu(x+anu)*U^dagger_nu(x)*/
+							//~ for (int nu =0;nu<4;nu+=1){
+								//~ if(mu!=nu){
+								//~ gsl_blas_zgemm(CblasNoTrans, CblasConjTrans, GSL_COMPLEX_ONE, matrixarray[counter-mu+neighbour[mu]+nu], matrixarray[counter+neighbour[nu]], GSL_COMPLEX_ZERO, helpone);
+								//~ gsl_blas_zgemm(CblasNoTrans, CblasConjTrans, GSL_COMPLEX_ONE, helpone, matrixarray[counter-mu+nu] , GSL_COMPLEX_ZERO, helptwo);
+								//~ gsl_matrix_complex_add(deltacontribution, helptwo);
 								//~ fprintf(stdout, "%.1f\t", GSL_REAL(det2(helptwo)));
-								if(mu>nu){gsl_matrix_complex_add(plaquettecontribution, helptwo);}
-							}}
+								//~ if(mu>nu){gsl_matrix_complex_add(plaquettecontribution, helptwo);}
+							//~ }}
+							calculateGamma(neighbour, counter, mu, size, plaquettecontribution, deltacontribution, helpone, helptwo, matrixarray);
 							for (int attempts=0;attempts<10;attempts+=1){
 								generatesu2(multiplier, epsilon, generator);
 								gsl_blas_zgemm(CblasNoTrans, CblasNoTrans, gsl_complex_rect(1,0), multiplier, matrixarray[counter], gsl_complex_rect(0,0), newmatrix);
 								//~ fprintf(stdout, "%.1f\t", GSL_REAL(det2(result)));
 								//beta in exp(-betaDeltaS)
-								if (exp(/*-1.0* */deltaS(plaquettecontribution, newmatrix, matrixarray[counter], helpone, helptwo))>gsl_rng_uniform(generator)){
+								if (exp(beta*deltaS(deltacontribution, newmatrix, matrixarray[counter], helpone, helptwo))>gsl_rng_uniform(generator)){
 									acceptance+=1;
 									gsl_matrix_complex_memcpy(matrixarray[counter], newmatrix); 
 								}
 								//what to use for plaquette: sum over (mu>nu) or 1/2*sum over (mu)
-								gsl_blas_zgemm(CblasNoTrans, CblasNoTrans, gsl_complex_rect(1,0), matrixarray[counter], deltacontribution, gsl_complex_rect(0,0), helpone);
-								plaquetteexpectation+=0.25*GSL_REAL(trace(helpone));
+								gsl_blas_zgemm(CblasNoTrans, CblasNoTrans, gsl_complex_rect(1,0), matrixarray[counter], plaquettecontribution, gsl_complex_rect(0,0), helpone);
+								plaquetteexpectation+=GSL_REAL(trace(helpone));
+							}
+						}
+					}
+				}
+			}
+		}	
+		for (int x=0;x<size;x+=1){
+			neighbour[6]=(x==size-1)?-(size-1)*pow(size, 3)*4:pow(size,3)*4;
+			neighbour[7]=(x==0)?(size-1)*pow(size, 3)*4:-pow(size,3)*4;
+			for (int y=0;y<size;y+=1){
+				neighbour[4]=(y==size-1)?-(size-1)*pow(size, 2)*4:pow(size,2)*4;
+				neighbour[5]=(y==0)?(size-1)*pow(size, 2)*4:-pow(size,2)*4;
+				for (int z=0;z<size;z+=1){
+					neighbour[2]=(z==size-1)?-(size-1)*size*4:size*4;
+					neighbour[3]=(z==0)?(size-1)*size*4:-size*4;
+					for (int t=0;t<size;t+=1){
+						neighbour[0]=(t==size-1)?-(size-1)*4:4;
+						neighbour[1]=(t==0)?(size-1)*4:-4;
+						counter=x*size*size*size*4+y*size*size*4+z*size*4+t*4;
+						for (int mu=0;mu<4;mu+=1){
+							for (int nu=mu+1;nu<4;nu+=1){
+								plaquetteafter+=calculateplaquette(matrixarray, counter, neighbour, mu, nu, helpone, helptwo, helpthree);
 							}
 						}
 					}
 				}
 			}
 		}
+							
 		//~ fprintf(stdout, "\nacceptance rate: %f\n", (double)acceptance/((double)10*size*size*size*size*4));
 		//~ fprintf(stdout, "\nplaquette expectation: %f\n", plaquetteexpectation/((double)10*size*size*size*size*4*3*0.5));
-		//~ fprintf(stdout, "%f\t%f\n", (double)acceptance/((double)10*size*size*size*size*4),plaquetteexpectation/((double)10*size*size*size*size*4*3*0.5));
+		fprintf(stdout, "%f\t%f\t%f\n", (double)acceptance/((double)10*size*size*size*size*4),plaquetteexpectation/((double)10*size*size*size*size*4*3), plaquetteafter/((double)size*size*size*size*4*3*0.5));
 	}
 	
 	//~ printf("measured\n");
 	
-	/**measuerements **/
-	for(int runs=0;runs<numberofmeasurements;runs+=1){
-		acceptance=0;
-		plaquetteexpectation=0;
-		for (int x=0;x<size;x+=1){
-			neighbour[0]=(x==size-1)?-(size-1)*pow(size, 3)*4:pow(size,3)*4;
-			for (int y=0;y<size;y+=1){
-				neighbour[1]=(y==size-1)?-(size-1)*pow(size, 2)*4:pow(size,2)*4;
-				for (int z=0;z<size;z+=1){
-					neighbour[2]=(z==size-1)?-(size-1)*size*4:size*4;
-					for (int t=0;t<size;t+=1){
-						neighbour[3]=(x==size-1)?-(size-1)*4:4;
-						for (int mu=0;mu<4;mu+=1){
-							counter=x*size*size*size*4+y*size*size*4+z*size*4+t*4+mu;
-							//~ fprintf(stdout, "%d\n", counter);
-							settozero(plaquettecontribution); settozero(deltacontribution);
-							/**calculate contribution of different plaquettes to action
-							*U_nu(x+amu)*U^dagger_mu(x+anu)*U^dagger_nu(x)*/
-							for (int nu =0;nu<4;nu+=1){
-								if(mu!=nu){
-								gsl_blas_zgemm(CblasNoTrans, CblasConjTrans, GSL_COMPLEX_ONE, matrixarray[counter-mu+neighbour[mu]+nu], matrixarray[counter+neighbour[nu]], GSL_COMPLEX_ZERO, helpone);
-								gsl_blas_zgemm(CblasNoTrans, CblasConjTrans, GSL_COMPLEX_ONE, helpone, matrixarray[counter-mu+nu] , GSL_COMPLEX_ZERO, helptwo);
-								gsl_matrix_complex_add(deltacontribution, helptwo);
-								//~ fprintf(stdout, "%.1f\t", GSL_REAL(det2(helptwo)));
-								if(mu>nu){gsl_matrix_complex_add(plaquettecontribution, helptwo);}
-							}}
-							for (int attempts=0;attempts<10;attempts+=1){
-								generatesu2(multiplier, epsilon, generator);
-								gsl_blas_zgemm(CblasNoTrans, CblasNoTrans, gsl_complex_rect(1,0), multiplier, matrixarray[counter], gsl_complex_rect(0,0), newmatrix);
-								//~ fprintf(stdout, "%.1f\t", GSL_REAL(det2(result)));
-								//beta in exp(-betaDeltaS)
-								if (exp(/*-1.0* */deltaS(plaquettecontribution, newmatrix, matrixarray[counter], helpone, helptwo))>gsl_rng_uniform(generator)){
-									acceptance+=1;
-									gsl_matrix_complex_memcpy(matrixarray[counter], newmatrix); 
-								}
-								//what to use for plaquette: sum over (mu>nu) or 1/2*sum over (mu)
-								gsl_blas_zgemm(CblasNoTrans, CblasNoTrans, gsl_complex_rect(1,0), matrixarray[counter], deltacontribution, gsl_complex_rect(0,0), helpone);
-								plaquetteexpectation+=0.25*GSL_REAL(trace(helpone));
-							}
-						}
-					}
-				}
-			}
-		}
-		//~ fprintf(stdout, "\nacceptance rate: %f\n", (double)acceptance/((double)10*size*size*size*size*4));
-		//~ fprintf(stdout, "\nplaquette expectation: %f\n", plaquetteexpectation/((double)10*size*size*size*size*4*3*0.5));
-		//~ fprintf(stdout, "%f\t%f\n", (double)acceptance/((double)10*size*size*size*size*4),plaquetteexpectation/((double)10*size*size*size*size*4*3*0.5));
-		gsl_vector_set(plaquette, runs, plaquetteexpectation/((double)10*size*size*size*size*4*3*0.5));
-	}
+	//~ /**measuerements **/
+	//~ for(int runs=0;runs<numberofmeasurements;runs+=1){
+		//~ acceptance=0;
+		//~ plaquetteexpectation=0;
+//~ //copy from thermalization
+		//~ gsl_vector_set(plaquette, runs, plaquetteexpectation/((double)10*size*size*size*size*4*3*0.5));
+	//~ }
 	
 	//~ printf("measured\n");
-	fprintf(plaquette_analysis, "bin\t<plaq>\tvar(plaq)\n", binsize, mean_plaquette, var_plaquette);
-	for (int binsize=1;binsize<33;binsize*=2){
-		binned_plaquette=gsl_vector_subvector(binned_plaquette_mem, 0, plaquette->size/binsize);
-		binning(plaquette, &binned_plaquette.vector, binsize);
+	//~ fprintf(plaquette_analysis, "bin\t<plaq>\tvar(plaq)\n", binsize, mean_plaquette, var_plaquette);
+	//~ for (int binsize=1;binsize<33;binsize*=2){
+		//~ binned_plaquette=gsl_vector_subvector(binned_plaquette_mem, 0, plaquette->size/binsize);
+		//~ binning(plaquette, &binned_plaquette.vector, binsize);
 		//~ printf("binned\n");
-		bootstrap(&binned_plaquette.vector, generator, 2, &mean_plaquette, &var_plaquette);
+		//~ bootstrap(&binned_plaquette.vector, generator, 2, &mean_plaquette, &var_plaquette);
 		//~ printf("bootstrapped\n");
-		autocorrelation(&binned_plaquette.vector, plaquette_correlation_binned, mean_plaquette);
+		//~ autocorrelation(&binned_plaquette.vector, plaquette_correlation_binned, mean_plaquette);
 		
-		fprintf(plaquette_data, "\nbinsize %d\n", binsize);
-		fprintf(plaquette_autocorrelation, "\nbinsize %d\n", binsize);
-		gsl_vector_fprintf(plaquette_data, &binned_plaquette.vector, "%f");
-		gsl_vector_fprintf(plaquette_autocorrelation, plaquette_correlation_binned, "%f");
+		//~ fprintf(plaquette_data, "\nbinsize %d\n", binsize);
+		//~ fprintf(plaquette_autocorrelation, "\nbinsize %d\n", binsize);
+		//~ gsl_vector_fprintf(plaquette_data, &binned_plaquette.vector, "%f");
+		//~ gsl_vector_fprintf(plaquette_autocorrelation, plaquette_correlation_binned, "%f");
 		
-		fprintf(plaquette_analysis, "%.2d\t%f\t%f\n", binsize, mean_plaquette, var_plaquette);
-	}
+		//~ fprintf(plaquette_analysis, "%.2d\t%f\t%f\n", binsize, mean_plaquette, var_plaquette);
+	//~ }
 	
 	
 	
