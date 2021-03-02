5abc5944c1ccc8bf6000806812d2ab6394d53763
TechnikforLife <nico.dichter@gmail.com>
2021-03-02 18:52:50 +0100

    Measurements for 2048 and acc 0.5

--- Project/src/main/main.c
+++ Project/src/main/main.c
@@ -227,7 +227,7 @@ double calculatewilsonloop(gsl_matrix_complex ** matrixarray, gsl_matrix_complex
 int main(int argc, char **argv){
 	//set up constants, matrices, generator
 	int dim=3; //switches between SU2 and SU3
-	double epsilon=0.2;
+	double epsilon=1.8;
 	int hotstart=1;
 	int size=8;
 	double beta=2.3;
@@ -282,6 +282,7 @@ int main(int argc, char **argv){
 	FILE * plaquette_data=fopen("data/plaquettedata.dat", "w");
 	FILE * plaquette_autocorrelation=fopen("data/plaquetteautocorrelation.dat", "w");
 	FILE * plaquette_analysis=fopen("data/plaquette.dat", "w");
+	FILE * wilson_data=fopen("data/wilson.dat", "w");
 	
 	//test of MH: go through lattice, perform 10 accept/reject steps at every link, measure acceptance rate
 	/**thermalizations**/
@@ -298,7 +299,7 @@ int main(int argc, char **argv){
 	 * **/
 	int counter, acceptance;
 	int neighbour[8]; //for implementing (periodic) boundary conditions
-	double plaquetteexpectation, plaquetteafter, wilsonexpectation[4], wilsonexpectationmedium;
+	double plaquetteexpectation, wilsonexpectation[4], wilsonexpectationmedium[4];
 	for(int runs=0;runs<numberofthermalizations;runs+=1){
 		acceptance=0;
 		plaquetteexpectation=0;
@@ -306,8 +307,6 @@ int main(int argc, char **argv){
 		wilsonexpectation[1]=0;
 		wilsonexpectation[2]=0;
 		wilsonexpectation[3]=0;
-		wilsonexpectationmedium=0;
-		plaquetteafter=0;
 		for (int x=0;x<size;x+=1){
 			neighbour[6]=(x==size-1)?-(size-1)*pow(size, 3)*4:pow(size,3)*4;
 			neighbour[7]=(x==0)?(size-1)*pow(size, 3)*4:-pow(size,3)*4;
@@ -352,9 +351,29 @@ int main(int argc, char **argv){
 				}
 			}
 		}	
-		/**measure plaquette after one sweep is complete**/
-		/** counter defined as position without direction**/
-		/**for (int x=0;x<size;x+=1){
+		/**where to measure plaquette? measure directly after one link is switched, and get contributions from links that are changed in the next step, or loop over entire lattice after every sweep and take longer?
+		Or maybe not longer, since matrix links are looked at ten times per sweep? Maybe look during sweep, but only after ten attempts have ben made?**/
+		/**factors for plaquette and acceptance rate: both have to be 1.0 when filled with unity matrices and epsilon=0**/
+		fprintf(stdout, "%d\tacc=%f\tplaq=%f\tw1=%f\tw2=%f\tw3=%f\tw4=%f\n", runs, (double)acceptance/((double)10*size*size*size*size*4),plaquetteexpectation/((double)10*size*size*size*size*4*3*dim/2),
+				/**plaquetteafter/((double)size*size*size*size*4*3*0.5),**/ wilsonexpectation[0]/((double)size*size*size*size),wilsonexpectation[1]/((double)size*size*size*size),wilsonexpectation[2]/((double)size*size*size*size),wilsonexpectation[3]/((double)size*size*size*size));
+	}
+
+
+	//~ /**measurements **/
+
+	for(int R=1;R<=size/2;R++){
+		wilsonexpectationmedium[0]=0;
+		wilsonexpectationmedium[1]=0;
+		wilsonexpectationmedium[2]=0;
+		wilsonexpectationmedium[3]=0;
+	for(int runs=0;runs<numberofmeasurements;runs+=1){
+		acceptance=0;
+		plaquetteexpectation=0;
+		wilsonexpectation[0]=0;
+		wilsonexpectation[1]=0;
+		wilsonexpectation[2]=0;
+		wilsonexpectation[3]=0;
+		for (int x=0;x<size;x+=1){
 			neighbour[6]=(x==size-1)?-(size-1)*pow(size, 3)*4:pow(size,3)*4;
 			neighbour[7]=(x==0)?(size-1)*pow(size, 3)*4:-pow(size,3)*4;
 			for (int y=0;y<size;y+=1){
@@ -366,32 +385,50 @@ int main(int argc, char **argv){
 					for (int t=0;t<size;t+=1){
 						neighbour[0]=(t==size-1)?-(size-1)*4:4;
 						neighbour[1]=(t==0)?(size-1)*4:-4;
-						counter=x*size*size*size*4+y*size*size*4+z*size*4+t*4;
 						for (int mu=0;mu<4;mu+=1){
-							for (int nu=mu+1;nu<4;nu+=1){
-								plaquetteafter+=calculateplaquette(matrixarray, counter, neighbour, mu, nu, helparray[0], helparray[1], helparray[2],dim);
+
+							counter=x*size*size*size*4+y*size*size*4+z*size*4+t*4+mu;
+							settozero(plaquettecontribution); settozero(deltacontribution);
+							calculateGamma(neighbour, counter, mu, size, plaquettecontribution, deltacontribution, helparray[0], helparray[1], matrixarray);
+							for (int attempts=0;attempts<10;attempts+=1){
+								if(dim==2){
+									generatesu2(multiplier, epsilon, generator);
+								}else if (dim==3){
+								 	generatesu3(multiplier, epsilon, generator);
+								}else {fprintf (stderr, "Invalid value for dim.");return -1;}
+
+								gsl_blas_zgemm(CblasNoTrans, CblasNoTrans, gsl_complex_rect(1,0), multiplier, matrixarray[counter], gsl_complex_rect(0,0), newmatrix);
+								if (exp(beta*deltaS(deltacontribution, newmatrix, matrixarray[counter], helparray[0], helparray[1],dim))>gsl_rng_uniform(generator)){
+									acceptance+=1;
+									gsl_matrix_complex_memcpy(matrixarray[counter], newmatrix);
+								}
+								/**what to use for plaquette: sum over (mu>nu) or 1/2*sum over (mu)? Also include backwards plaquettes?**/
+								gsl_blas_zgemm(CblasNoTrans, CblasNoTrans, gsl_complex_rect(1,0), matrixarray[counter], plaquettecontribution, gsl_complex_rect(0,0), helparray[0]);
+								plaquetteexpectation+=GSL_REAL(trace(helparray[0]));
+
 							}
 						}
+
+						wilsonexpectation[0]+=calculatewilsonloop(matrixarray, helparray, x, y, z, t, R, 0,0,1,size,dim);
+						wilsonexpectation[1]+=calculatewilsonloop(matrixarray, helparray, x, y, z, t, R, 0,0,2,size,dim);
+						wilsonexpectation[2]+=calculatewilsonloop(matrixarray, helparray, x, y, z, t, R, 0,0,3,size,dim);
+						wilsonexpectation[3]+=calculatewilsonloop(matrixarray, helparray, x, y, z, t, R, 0,0,4,size,dim);
 					}
 				}
 			}
-		}**/
+		}
 		/**where to measure plaquette? measure directly after one link is switched, and get contributions from links that are changed in the next step, or loop over entire lattice after every sweep and take longer? 
 		Or maybe not longer, since matrix links are looked at ten times per sweep? Maybe look during sweep, but only after ten attempts have ben made?**/
 		/**factors for plaquette and acceptance rate: both have to be 1.0 when filled with unity matrices and epsilon=0**/
+		wilsonexpectationmedium[0]+=wilsonexpectation[0]/((double)size*size*size*size);
+		wilsonexpectationmedium[1]+=wilsonexpectation[1]/((double)size*size*size*size);
+		wilsonexpectationmedium[2]+=wilsonexpectation[2]/((double)size*size*size*size);
+		wilsonexpectationmedium[3]+=wilsonexpectation[3]/((double)size*size*size*size);
 		fprintf(stdout, "%d\tacc=%f\tplaq=%f\tw1=%f\tw2=%f\tw3=%f\tw4=%f\n", runs, (double)acceptance/((double)10*size*size*size*size*4),plaquetteexpectation/((double)10*size*size*size*size*4*3*dim/2),
-				/**plaquetteafter/((double)size*size*size*size*4*3*0.5),**/ wilsonexpectation[0]/((double)size*size*size*size),wilsonexpectation[1]/((double)size*size*size*size),wilsonexpectation[2]/((double)size*size*size*size),wilsonexpectation[3]/((double)size*size*size*size));
+				/**plaquetteafter/((double)size*size*size*size*4*3*0.5),**/ wilsonexpectationmedium[0]/((double)(runs+1)),wilsonexpectationmedium[1]/((double)(runs+1)),wilsonexpectationmedium[2]/((double)(runs+1)),wilsonexpectationmedium[3]/((double)(runs+1)));
 	}
-	
-	
-	//~ /**measurements **/
-	//~ for(int runs=0;runs<numberofmeasurements;runs+=1){
-		//~ acceptance=0;
-		//~ plaquetteexpectation=0;
-/**copy from thermalization-> still to be done, but set up functions for gamma, plaquette, wilson-loop, etc first**/
-		//~ gsl_vector_set(plaquette, runs, plaquetteexpectation/((double)10*size*size*size*size*4*3*0.5));
-	//~ }
-	
+		fprintf(wilson_data, "%d\t%f\t%f\t%f\t%f\n",R,wilsonexpectationmedium[0]/((double)(numberofmeasurements)),wilsonexpectationmedium[1]/((double)(numberofmeasurements)),wilsonexpectationmedium[2]/((double)(numberofmeasurements)),wilsonexpectationmedium[3]/((double)(numberofmeasurements)));
+	}
 	/** analysis by binning and bootstrapping**/
 	//~ fprintf(plaquette_analysis, "bin\t<plaq>\tvar(plaq)\n", binsize, mean_plaquette, var_plaquette);
 	//~ for (int binsize=1;binsize<33;binsize*=2){
@@ -408,24 +445,6 @@ int main(int argc, char **argv){
 		//~ fprintf(plaquette_analysis, "%.2d\t%f\t%f\n", binsize, mean_plaquette, var_plaquette);
 	//~ }
 	
-	//Test SU3:
-	gsl_matrix_complex *su3=gsl_matrix_complex_alloc(3,3);
-	gsl_vector_complex_view columnzero,columnone,columntwo;
-	double norm;
-	gsl_complex dot01, dot02, dot12;
-	for(int i=0;i<50;i++){
-		generatesu3 (su3, i/50., generator);
-		//gsl_matrix_complex_fprintf (stdout, su3, "%e");
-		columnzero=gsl_matrix_complex_column(su3, 0);
-		columnone=gsl_matrix_complex_column(su3, 1);
-		columntwo=gsl_matrix_complex_column(su3, 2);
-		gsl_blas_zdotc (&columnzero.vector, &columnone.vector, &dot01);
-		gsl_blas_zdotc (&columnzero.vector, &columntwo.vector, &dot02);
-		gsl_blas_zdotc (&columnone.vector, &columntwo.vector, &dot12);
-		norm=gsl_blas_dznrm2(&columntwo.vector);
-		printf ("eps: %e\tnorm: %e\tdot01: %e\tdot02: %e\tdot12: %e\n",i/50.,norm,gsl_complex_abs (dot01),gsl_complex_abs (dot02),gsl_complex_abs (dot12));
-		gsl_matrix_complex_set_zero (su3);
-	}
 
 
 
@@ -453,6 +472,7 @@ int main(int argc, char **argv){
 	fclose(plaquette_data);
 	fclose(plaquette_autocorrelation);
 	fclose(plaquette_analysis);
+	fclose (wilson_data);
 	return 0;
 }
 
